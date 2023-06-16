e13eb64d3ba755018175c8c5b960f8db284409e3
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-16 16:47:03 +0900

    [Feat]공갈빵 game model

---
+++ src/domain/factory/model/game.model.ts
@@ -0,0 +1,14 @@
+import { GameMode } from 'src/global/type/type.game.mode';
+import { v4 as uuid } from 'uuid';
+
+export class GameModel {
+  id: string;
+  mode: GameMode;
+  createdAt: Date;
+
+  constructor(id: string, mode: GameMode) {
+    this.id = uuid();
+    this.mode = mode;
+    this.createdAt = new Date();
+  }
+}
