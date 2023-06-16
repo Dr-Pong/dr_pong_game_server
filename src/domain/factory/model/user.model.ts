2419458e64d60b11baeb666e8954de2de6cab43a
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-16 12:24:30 +0900

    [Feat] User Model #3

---
+++ src/domain/factory/model/user.model.ts
@@ -0,0 +1,37 @@
+import { Socket } from 'socket.io';
+import {
+  USERSTATUS_OFFLINE,
+  UserStatusType,
+} from 'src/global/type/type.user.status';
+import { InviteModel } from './invite.model';
+import { User } from 'src/domain/user/user.entity';
+
+export class UserModel {
+  id: number;
+  nickname: string;
+  ladderPoint: number;
+  socket: Socket;
+  invite: InviteModel;
+  inviteList: Map<string, InviteModel>;
+  status: UserStatusType;
+
+  static fromEntity(entity: User): UserModel {
+    const { id, nickname, ladderPoint } = entity;
+    return new UserModel(id, nickname, ladderPoint, null);
+  }
+
+  constructor(
+    id: number,
+    nickname: string,
+    ladderPoint: number,
+    socket: Socket,
+  ) {
+    this.id = id;
+    this.nickname = nickname;
+    this.ladderPoint = ladderPoint;
+    this.socket = socket;
+    this.invite = null;
+    this.inviteList = new Map();
+    this.status = USERSTATUS_OFFLINE;
+  }
+}
---
+++ src/global/type/type.user.status.ts
@@ -0,0 +1,4 @@
+export const USERSTATUS_ONLINE = 'online' as const;
+export const USERSTATUS_OFFLINE = 'offline' as const;
+
+export type UserStatusType = 'online' | 'offline';
