d39a7d5ae758c9a84b8ff943fbbc5ea1543dd065
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-21 14:28:47 +0900

    [Fix] set 안해도 되는 부분 제거 #4

--- src/domain/factory/user.factory.ts
+++ src/domain/factory/user.factory.ts
@@ -24,20 +24,16 @@ export class UserFactory {
   setStatus(userId: number, status: UserStatusType): void {
     const user: UserModel = this.findById(userId);
     user.status = status;
-    this.users.set(user.id, user);
   }
 
   invite(userId: number, invite: InviteModel): void {
     const user: UserModel = this.findById(userId);
     user.invite = invite;
-    this.users.set(user.id, user);
   }
 
   uninvite(sender: UserModel, receiver: UserModel): void {
     sender.invite = null;
     receiver.inviteList.delete(sender.invite.id);
-    this.users.set(sender.id, sender);
-    this.users.set(receiver.id, receiver);
   }
 
   getInvites(userId: number): InviteModel[] {
@@ -48,6 +44,5 @@ export class UserFactory {
   deleteInvite(userId: number, gameId: string): void {
     const user: UserModel = this.findById(userId);
     user.inviteList.delete(gameId);
-    this.users.set(user.id, user);
   }
 }
