44779f379cbf4bb9426ab612c3ec6c5de2338e34
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-22 11:42:24 +0900

    [Fix] 큐 자료구조 수정 #5

--- src/domain/factory/queue.factory.ts
+++ src/domain/factory/queue.factory.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class QueueFactory {
-  private ladderQueue: Map<number, number[]> = new Map();
+  private ladderQueue: Map<number, number> = new Map();
 
   addLadderQueue(userId: number): void {
-    const queue = this.ladderQueue.get(userId) || [];
-    this.ladderQueue.set(userId, queue);
+    this.ladderQueue.set(userId, userId);
   }
 
-  get(userId: number): number[] {
+  get(userId: number): number {
     return this.ladderQueue.get(userId);
   }
 
