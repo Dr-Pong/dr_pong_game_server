438b08055f74ce5924a5aaeceb7853be061425d9
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-22 10:17:31 +0900

    [Feat] queue컨트롤러 조립 #5

--- src/domain/game/game.controller.ts
+++ src/domain/game/game.controller.ts
@@ -8,11 +8,16 @@ import { PostGameInviteDto } from './dto/post.game.invite.dto';
 import { DeleteGameInviteDto } from './dto/delete.game.invite.dto';
 import { PostGameInviteAcceptDto } from './dto/post.game.invite.accept.dto';
 import { DeleteGameInviteRejectDto } from './dto/delete.game.invite.reject.dto';
+import { QueueService } from '../queue/queue.service';
+import { GameType } from 'src/global/type/type.game.type';
+import { PostQueueDto } from '../queue/dto/post.queue.dto';
+import { DeleteQueueDto } from '../queue/dto/delete.queue.dto';
 
 @Controller('/games')
 export class GameController {
   constructor(
     private readonly gameService: GameService,
+    private readonly queueService: QueueService,
     private readonly userFactory: UserFactory,
   ) {}
 
@@ -57,5 +62,23 @@ export class GameController {
     const { id: userId } = requestor;
     const deleteDto = new DeleteGameInviteRejectDto(userId, id);
     await this.gameService.deleteGameInviteReject(deleteDto);
+  }
+
+  @Post('/queue/:type/:mode')
+  async gameQueuePost(
+    @Requestor() requestor: UserIdCardDto,
+    @Param('type') type: GameType,
+    @Param('mode') mode: GameMode,
+  ): Promise<void> {
+    const { id: userId } = requestor;
+    const postDto = new PostQueueDto(userId, type, mode);
+    await this.queueService.postQueue(postDto);
+  }
+
+  @Delete('/queue')
+  async gameQueueDelete(@Requestor() requestor: UserIdCardDto): Promise<void> {
+    const { id: userId } = requestor;
+    const deleteDto = new DeleteQueueDto(userId);
+    await this.queueService.deleteQueue(deleteDto);
   }
 }
