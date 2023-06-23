d36c86993ee8ace959d04222989814d1d59e5225
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-23 17:04:13 +0900

    [Feat] Controller 모듈 추가 #11

--- src/domain/game/game.module.ts
+++ src/domain/game/game.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GameService } from './game.service';
 import { FactoryModule } from '../factory/factory.module';
+import { GameController } from './game.controller';
 
 @Module({
   imports: [FactoryModule],
   providers: [GameService],
   exports: [GameService],
+  controllers: [GameController],
 })
 export class GameModule {}
