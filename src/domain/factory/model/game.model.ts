c7373a0e3e4457b03d10a1e3351b9ea2d671b73e
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-27 17:29:07 +0900

    [Fix] 게임 모델 추가 #5

--- src/domain/factory/model/game.model.ts
+++ src/domain/factory/model/game.model.ts
@@ -4,11 +4,15 @@ import { v4 as uuid } from 'uuid';
 export class GameModel {
   id: string;
   mode: GameMode;
+  player1: number;
+  player2: number;
   createdAt: Date;
 
-  constructor(mode: GameMode) {
+  constructor(player1: number, player2: number, mode: GameMode) {
     this.id = uuid();
     this.mode = mode;
+    this.player1 = player1;
+    this.player2 = player2;
     this.createdAt = new Date();
   }
 }
