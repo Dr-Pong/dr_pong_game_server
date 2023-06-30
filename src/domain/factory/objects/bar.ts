fc1456eda14bbdde1f8b13fb193e62ded2be8885
nheo9143 <nheo@student.42seoul.kr>
2023-06-30 19:18:56 +0900

    [Fix] Bar 객체에 탄성, 마찰 추가 #19

--- src/domain/factory/objects/bar.ts
+++ src/domain/factory/objects/bar.ts
@@ -1,3 +1,5 @@
+import { randomInt } from 'crypto';
+
 export type BarMode = 'normal' | 'special';
 export type BarDirection = 'left' | 'right' | 'stop';
 
@@ -7,18 +9,42 @@ export class Bar {
   width: number;
   position: number;
   mode: BarMode;
+  // 탄성계수
+  elasticity: number;
+  // 마찰계수
+  friction: number;
 
   constructor(speed: number, width: number, position: number) {
     this.speed = speed;
     this.width = width;
     this.position = position;
+    this.direction = randomInt(2) % 2 === 0 ? 'left' : 'right'; // 콘솔 테스트용
+    // this.direction = 'stop'; // 실제 쓸거
+    this.mode = 'normal';
+    this.elasticity = 1;
+    this.friction = randomInt(20, 50) / 10; // 0.5 ~ 1.5
   }
 
   reset(): void {
-    this.direction = 'stop';
+    this.direction = randomInt(2) % 2 === 0 ? 'left' : 'right'; // 콘솔 테스트용
+    // this.direction = 'stop'; // 실제 쓸거
+    this.position = +process.env.BOARD_WIDTH / 2;
   }
 
   move(): void {
+    // 벽 충돌 처리
+    if (this.position <= this.width / 2 && this.direction === 'left') {
+      this.direction = 'right'; // 콘솔 테스트용
+      // return;
+    }
+    if (
+      this.position >= +process.env.BOARD_WIDTH - this.width / 2 &&
+      this.direction === 'right'
+    ) {
+      this.direction = 'left'; // 콘솔 테스트용
+      // return;
+    }
+
     if (this.direction === 'left') {
       this.moveLeft();
     } else if (this.direction === 'right') {
@@ -27,10 +53,10 @@ export class Bar {
   }
 
   moveLeft(): void {
-    this.position -= this.speed;
+    this.position -= this.speed / +process.env.GAME_FRAME;
   }
 
   moveRight(): void {
-    this.position += this.speed;
+    this.position += this.speed / +process.env.GAME_FRAME;
   }
 }
