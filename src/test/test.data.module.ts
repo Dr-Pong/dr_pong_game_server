0a96afa2090a5d040adc2a7ad72fb226c8aa4054
nheo9143 <nheo@student.42seoul.kr>
2023-07-17 20:27:05 +0900

    [Refactor] 게임 초대 알림 관련 코드 제거 #46

--- src/test/test.data.module.ts
+++ src/test/test.data.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/domain/auth/auth.module';
 import { User } from 'src/domain/user/user.entity';
-import { GameTestData } from './data/game.test.data';
 import { UserTestData } from './data/user.test.data';
 import { FactoryModule } from 'src/domain/factory/factory.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), FactoryModule, AuthModule],
-  providers: [GameTestData, UserTestData],
-  exports: [GameTestData, UserTestData],
+  providers: [UserTestData],
+  exports: [UserTestData],
 })
 export class TestDataModule {}
