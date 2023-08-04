7d00312418c114e59eae3567789a4917f7fe0075
nheo9143 <nheo@student.42seoul.kr>
2023-08-04 15:52:50 +0900

    [Feat] Redis Adapter 적용, RedisRepositoryModule 추가 #66

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import { RedisIoAdapter } from './global/utils/redis.socket.adapter';
 
 async function bootstrap() {
   initializeTransactionalContext();
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+  app.useWebSocketAdapter(redisIoAdapter);
   await app.listen(4343);
 }
 bootstrap();
