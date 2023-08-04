0b2d06d3fe7fc0da5530780c42d58778d91b75bb
nheo9143 <nheo@student.42seoul.kr>
2023-08-04 20:07:56 +0900

    [Fix] 컴파일 수정 #66

--- src/domain/gateway/gateway.module.ts
+++ src/domain/gateway/gateway.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { GameGateWay } from './game.gateway';
 import { QueueGateWay } from './queue.gateway';
 import { FactoryModule } from '../factory/factory.module';
-import { TestGateWay } from './test.gateway';
 import { RedisRepositoryModule } from '../redis/redis.repository.module';
 import { MutexModule } from '../mutex/mutex.module';
 
 @Module({
   imports: [RedisRepositoryModule, FactoryModule, MutexModule],
-  providers: [GameGateWay, QueueGateWay, TestGateWay],
+  providers: [GameGateWay, QueueGateWay],
   exports: [GameGateWay, QueueGateWay],
 })
 export class GatewayModule {}
