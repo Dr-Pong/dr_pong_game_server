50068091aae908763f75d1accf2bf5072f0a54a8
nheo9143 <nheo@student.42seoul.kr>
2023-08-17 20:13:08 +0900

    [Fix] 다중 소켓 검사 조건문 수정 #76

--- src/domain/gateway/queue.gateway.ts
+++ src/domain/gateway/queue.gateway.ts
@@ -34,11 +34,12 @@ export class QueueGateWay implements OnGatewayConnection, OnGatewayDisconnect {
       if (!user) {
         console.log('user not found');
         socket.disconnect();
+        release();
         return;
       }
       console.log('join queue', user.nickname);
 
-      if (user.queueSocket !== socket.id) {
+      if (user.queueSocket && user.queueSocket !== socket.id) {
         this.server.in(user.queueSocket).disconnectSockets(true);
         release();
         return;
