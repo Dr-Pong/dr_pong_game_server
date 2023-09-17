f51191fcc0fa6b3222eab9972b28565b9d9bc20c
nheo9143 <nheo@student.42seoul.kr>
2023-09-17 18:36:59 +0900

    [Fix] 프레임 정보 배열로 전송하도록 수정 #82

--- src/domain/game/dto/game.pos.update.dto.ts
+++ src/domain/game/dto/game.pos.update.dto.ts
@@ -1,37 +1,83 @@
 import { GameModel } from 'src/domain/factory/model/game.model';
 import * as dotenv from 'dotenv';
+import { Ball } from 'src/domain/factory/objects/ball';
+import { Bar } from 'src/domain/factory/objects/bar';
 
 dotenv.config();
 
-export class GamePosUpdateDto {
+class GamePosDto {
   ballPos: { x: number; y: number };
   playerXPos: {
     me: number;
     opponent: number;
   };
   gameTime: number;
+  frame: number;
+
+  constructor(
+    ballPos: { x: number; y: number },
+    playerXPos: {
+      me: number;
+      opponent: number;
+    },
+    gameTime: number,
+    frame: number,
+  ) {
+    this.ballPos = ballPos;
+    this.playerXPos = playerXPos;
+    this.gameTime = gameTime;
+    this.frame = frame;
+  }
+}
+
+export class GamePosUpdateDto {
+  data: GamePosDto[] = [];
 
   constructor(game: GameModel, userId: number) {
-    if (game.player1.id === userId) {
-      this.ballPos = {
-        x: game.ball.x / game.board.width,
-        y: game.ball.y / game.board.height,
-      };
-      this.playerXPos = {
-        me: game.player1.bar.position / game.board.width,
-        opponent: game.player2.bar.position / game.board.width,
-      };
-    } else {
-      this.ballPos = {
-        x: (game.board.width - game.ball.x) / game.board.width,
-        y: (game.board.height - game.ball.y) / game.board.height,
-      };
-      this.playerXPos = {
-        me: (game.board.width - game.player2.bar.position) / game.board.width,
-        opponent:
-          (game.board.width - game.player1.bar.position) / game.board.width,
-      };
+    const tempBall: Ball = game.ball.copy();
+    const tempPlayer1Bar: Bar = game.player1.bar.copy();
+    const tempPlayer2Bar: Bar = game.player2.bar.copy();
+
+    for (let i = 0; i < 20; i++) {
+      const gameTime =
+        +process.env.GAME_TIME -
+        (game.playTime + (i * 1000) / +process.env.GAME_FRAME);
+      if (game.player1.id === userId) {
+        const ballPos = {
+          x: tempBall.x / game.board.width,
+          y: tempBall.y / game.board.height,
+        };
+        const playerXPos = {
+          me: tempPlayer1Bar.position / game.board.width,
+          opponent: tempPlayer2Bar.position / game.board.width,
+        };
+        this.data.push(
+          new GamePosDto(ballPos, playerXPos, gameTime, game.frame + i),
+        );
+      } else {
+        const ballPos = {
+          x: (game.board.width - tempBall.x) / game.board.width,
+          y: (game.board.height - tempBall.y) / game.board.height,
+        };
+        const playerXPos = {
+          me: (game.board.width - tempPlayer2Bar.position) / game.board.width,
+          opponent:
+            (game.board.width - tempPlayer1Bar.position) / game.board.width,
+        };
+        this.data.push(
+          new GamePosDto(ballPos, playerXPos, gameTime, game.frame + i),
+        );
+      }
+      tempBall.move();
+      tempPlayer1Bar.move();
+      tempPlayer2Bar.move();
+      if (tempBall.x - tempBall.size / 2 <= 0) tempBall.touchWall();
+      if (tempBall.x + tempBall.size / 2 >= game.board.width)
+        tempBall.touchWall();
+      if (tempBall.isTouchingBar(tempPlayer1Bar))
+        tempBall.touchBar(tempPlayer1Bar);
+      if (tempBall.isTouchingBar(tempPlayer2Bar))
+        tempBall.touchBar(tempPlayer2Bar);
     }
-    this.gameTime = +process.env.GAME_TIME - game.playTime;
   }
 }
