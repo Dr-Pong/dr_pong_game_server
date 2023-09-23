5593fca2d4ce7e4c1c335919af672f1e343ba03b
nheo9143 <nheo@student.42seoul.kr>
2023-09-23 23:39:31 +0900

    [Refactor] 사용하지 않는 postgres 관련 코드 제거 #96

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 import { FactoryModule } from '../factory/factory.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
 import { RedisRepositoryModule } from '../redis/redis.repository.module';
 
 @Module({
-  imports: [
-    FactoryModule,
-    TypeOrmModule.forFeature([User]),
-    RedisRepositoryModule,
-  ],
-  providers: [UserRepository, UserService],
+  imports: [FactoryModule, RedisRepositoryModule],
+  providers: [UserService],
   exports: [UserService],
 })
 export class UserModule {}
--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -1,28 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import {
-  IsolationLevel,
-  Transactional,
-  runOnTransactionComplete,
-} from 'typeorm-transactional';
-import { User } from './user.entity';
 import { UserModel } from '../factory/model/user.model';
 import { PostGatewayUserDto } from './dto/post.gateway.users.dto';
-import { UserRepository } from './user.repository';
 import { RedisUserRepository } from '../redis/redis.user.repository';
 
 @Injectable()
 export class UserService {
-  constructor(
-    private readonly redisUserRepository: RedisUserRepository,
-    private readonly userRepository: UserRepository,
-  ) {}
+  constructor(private readonly redisUserRepository: RedisUserRepository) {}
 
-  @Transactional({ isolationLevel: IsolationLevel.SERIALIZABLE })
   async postUser(postDto: PostGatewayUserDto): Promise<void> {
-    const user: User = await this.userRepository.save(postDto);
-
-    runOnTransactionComplete(async () => {
-      await this.redisUserRepository.create(UserModel.fromEntity(user));
-    });
+    await this.redisUserRepository.create(
+      new UserModel(postDto.id, postDto.nickname),
+    );
   }
 }
